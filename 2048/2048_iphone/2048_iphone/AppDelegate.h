51839dc73729c3fad75d9f52e5b5da6ce89b2632
13 <vi@mail.ru>
2015-04-06 22:21:51 +0300

    first commit

---
+++ 2048/2048_iphone/2048_iphone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2048_iphone
+//
+//  Created by admin on 06.04.15.
+//  Copyright (c) 2015 Vika Vovk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2048/2048_iphone/2048_iphone/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  2048_iphone
+//
+//  Created by admin on 06.04.15.
+//  Copyright (c) 2015 Vika Vovk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
